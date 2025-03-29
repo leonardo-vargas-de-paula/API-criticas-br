b6ccc39a3a66f2363019ca60afc8ae92acdf4ecf
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2025-03-29 10:59:33 -0400

    primeiro commit

---
+++ api/src/test/java/com/criticasbr/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.criticasbr.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
